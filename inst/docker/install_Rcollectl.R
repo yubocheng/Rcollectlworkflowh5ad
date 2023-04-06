b329ae3207e57bd51265411d47354e4e28f9dd56
Yubo Cheng <yubo.cheng@roswellpark.org>
2023-04-06 08:20:26 -0400

    update workflow_cellxgene_h5ad.Rmd

--- inst/docker/install_Rcollectl.R
+++ inst/docker/install_Rcollectl.R
@@ -1,10 +1,13 @@
 #! /usr/bin/env Rscript
 
 # Install required packages
-BiocManager::install(c("ggplot2", "dplyr", "pheatmap", "sessioninfo", "devtools", "GenomicFeatures", "tidyr", "cellxgenedp", "zellkonverter", "HDF5Array", "matrixStats", "SingleCellExperiment", "ensembldb", "EnsDb.Hsapiens.v86", "scater",  "BiocParallel", "scran", "batchelor", "BiocNeighbors", "uwot", "bluster", "celldex", "SingleR", "rmarkdown", "BiocStyle"))
+BiocManager::install(c("ggplot2", "dplyr", "pheatmap", "sessioninfo", "devtools", "GenomicFeatures", "tidyr", "cellxgenedp", "HDF5Array", "matrixStats", "SingleCellExperiment", "ensembldb", "EnsDb.Hsapiens.v86", "scater",  "BiocParallel", "scran", "batchelor", "BiocNeighbors", "uwot", "bluster", "celldex", "SingleR", "rmarkdown", "BiocStyle"))
 
 # Install Rcollectl
 BiocManager::install("vjcitn/Rcollectl")
+
+# Install zellkonverter
+BiocManager::install("mtmorgan/zellkonverter", ref = "include-native-dimnames")
 
 # Install workflow vignette
 BiocManager::install("yubocheng/Rcollectlworkflowh5ad", build_vignettes = TRUE)
