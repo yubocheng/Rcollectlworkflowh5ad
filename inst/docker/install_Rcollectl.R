2355b9c8b7a46102074a3cbfd1080f84ebc2667b
Yubo Cheng <yubo.cheng@roswellpark.org>
2023-05-18 08:05:12 -0400

    update workflow_cellxgene_h5ad.Rmd and docker

--- inst/docker/install_Rcollectl.R
+++ inst/docker/install_Rcollectl.R
@@ -1,13 +1,18 @@
 #! /usr/bin/env Rscript
 
 # Install required packages
-BiocManager::install(c("ggplot2", "dplyr", "pheatmap", "sessioninfo", "devtools", "GenomicFeatures", "tidyr", "cellxgenedp", "HDF5Array", "matrixStats", "SingleCellExperiment", "ensembldb", "EnsDb.Hsapiens.v86", "scater",  "BiocParallel", "scran", "batchelor", "BiocNeighbors", "uwot", "bluster", "celldex", "SingleR", "rmarkdown", "BiocStyle"))
+BiocManager::install(c("ggplot2", "dplyr", "pheatmap", "sessioninfo", "devtools", "GenomicFeatures", "cellxgenedp", "HDF5Array", "SingleCellExperiment", "EnsDb.Hsapiens.v86", "scater",  "BiocParallel", "scran", "batchelor", "BiocNeighbors", "uwot", "bluster", "celldex", "SingleR", "rmarkdown", "BiocStyle"))
+
+devtools::install_github("jimhester/scater")
+devtools::install_github("LTLA/scuttle")
+devtools::install_github("MarioniLab/scran")
 
 # Install Rcollectl
 BiocManager::install("vjcitn/Rcollectl")
 
 # Install zellkonverter
-BiocManager::install("mtmorgan/zellkonverter", ref = "include-native-dimnames")
+#BiocManager::install("mtmorgan/zellkonverter", ref = "include-native-dimnames")
+devtools::install_github("mtmorgan/zellkonverter", ref = "include-native-dimnames")
 
 # Install workflow vignette
 BiocManager::install("yubocheng/Rcollectlworkflowh5ad", build_vignettes = TRUE)
